ef9d63cda80a4b6e93f8754b7fd414a44b58ea75
xeusteerapat <xeus085@gmail.com>
2021-01-24 15:38:39 +0700

    [EDIT] email and password validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send({
-    route: 'user signup',
-  });
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating a user');
+
+    res.send({
+      success: true,
+    });
+  }
+);
 
 export { router as signupRouter };
