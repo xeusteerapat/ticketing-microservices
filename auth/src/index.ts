121c6b3ef3f32c34a130ecf8a6bdf9af480a2fb6
xeusteerapat <xeus085@gmail.com>
2021-01-24 23:17:02 +0700

    [EDIT] wire up error handling logic

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -12,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Authentication service is listening on port 3000!!!!');
